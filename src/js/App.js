8447e6a784210c30eb98a603eba4da9fd0b8b734
jellywiz <muzaffer.dler@gmail.com>
2022-11-21 23:45:09 +0300

    Update App.js to use displayMovies function

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
-import displayImage from './module/displayImg.js';
+import { displayImage, Displaymovies } from './module/displayImg.js';
 import '../style.css';
 
 document.addEventListener('DOMContentLoaded', displayImage(0, 'image'));
+document.addEventListener('DOMContentLoaded', Displaymovies(12));
