ba5095a9eae84bce76cd423ab1c36bcb315ae25e
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-21 13:23:52 -0800

    Made popup design/functionality (demo)

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 import displayImage from './module/displayImg.js';
+import {openModal, closeModal} from './module/popup.js'
 import '../style.css';
 
 document.addEventListener('DOMContentLoaded', displayImage(0, 'image'));
