e5544b48d9247245c3fec1744e54c66259e934bc
jellywiz <muzaffer.dler@gmail.com>
2022-11-22 19:03:39 +0300

    Update the Api

--- src/js/module/InvolmentApi.js
+++ src/js/module/InvolmentApi.js
@@ -2,6 +2,7 @@ export default class InvolvementAPI {
   constructor() {
     this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
     this.key = 'uTwt6FRnbbTssMmx6qrV';
+    this.headers = { 'Content-type': 'application/json; charset=UTF-8' };
   }
 
   async getId() {
@@ -12,45 +13,43 @@ export default class InvolvementAPI {
     return data;
   }
 
-  async getLikes() {
-    const response = await fetch(`${this.url}${this.key}/likes`);
-    const data = await response.json();
-    return data;
-  }
-
-  async postLikes() {
+  async addLikes(id) {
     const response = await fetch(`${this.url}${this.key}/likes`, {
       method: 'POST',
       body: JSON.stringify({
-        item_id: '1',
+        item_id: id,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
+    return response;
+  }
+
+  async getLikes() {
+    const result = await fetch(`${this.url}${this.key}/likes`, {
+      method: 'GET',
+    });
+    const list = await result.json();
+    return list;
+  }
+
+  async getComments(id) {
+    const response = await fetch(`${this.url}${this.key}/comments?item_id=${id}`, { method: 'GET' });
     const data = await response.json();
     return data;
   }
 
-  async getComments() {
-    const response = await fetch(`${this.url}${this.key}/comments`);
-    const data = await response.json();
-    return data;
-  }
-
-  async postComments(comment) {
-    const response = await fetch(`${this.url}${this.key}/comments`, {
+  async postComments(comment, UserName, id) {
+    const body = JSON.stringify({
+      item_id: id,
+      username: UserName,
+      comment,
+    });
+    await fetch(`${this.url}${this.key}/comments`, {
       method: 'POST',
-      body: JSON.stringify({
-        item_id: '1',
-        username: 'user',
-        comment,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
+      body,
+      headers: this.headers,
     });
-    const data = await response.json();
-    return data;
   }
 }
