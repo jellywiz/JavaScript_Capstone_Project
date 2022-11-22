4623196e5e334830b9626dcda03e278b785a423e
jellywiz <muzaffer.dler@gmail.com>
2022-11-22 17:43:55 +0300

    Creating Api class with constructor

---
+++ src/js/module/InvolmentApi.js
@@ -0,0 +1,6 @@
+export default class InvolvementAPI {
+  constructor() {
+    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+    this.key = 'uTwt6FRnbbTssMmx6qrV';
+  }
+}
