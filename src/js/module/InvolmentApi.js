d5e5656e1f5fabeb923960f13bf48a20620cb997
jellywiz <muzaffer.dler@gmail.com>
2022-11-22 17:44:31 +0300

    add fetching for id

--- src/js/module/InvolmentApi.js
+++ src/js/module/InvolmentApi.js
@@ -3,4 +3,12 @@ export default class InvolvementAPI {
     this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
     this.key = 'uTwt6FRnbbTssMmx6qrV';
   }
+
+  async getId() {
+    const response = await fetch(`${this.url}`, {
+      method: 'POST',
+    });
+    const data = await response.text();
+    return data;
+  }
 }
