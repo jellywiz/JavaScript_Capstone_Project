a2cce12679f05ea55b56bb83cd6c49e8b4e3b94a
jellywiz <muzaffer.dler@gmail.com>
2022-11-22 11:45:55 +0300

    Remove comments

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,7 @@
 import '../style.css';
 import displayImage from './module/displayImg.js';
 import displayMovies from './module/displaymovies.js';
-// import { openModal, closeModal } from './module/popup.js';
 import { displayModal } from './module/displayModal.js';
-
-// document.addEventListener('DOMContentLoaded', displayImage(0, 'image'));
-// document.addEventListener('DOMContentLoaded', Displaymovies(12));
 
 const moviesContainer = document.querySelector('.main-films');
 document.onload = displayImage(0);
