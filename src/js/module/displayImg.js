7fb4e619438299e1ff3a28b57eae5184b34c1525
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-24 06:56:29 -0800

    Updated design

--- src/js/module/displayImg.js
+++ src/js/module/displayImg.js
@@ -3,5 +3,4 @@ import { imageContainer } from './elements.js';
 
 export default async function displayImage(number) {
   const data = await MovieApi.fetchallmovies();
-  imageContainer.innerHTML += `<img src="${data[number].image}" />`;
 }
