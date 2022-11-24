5ed701faab24f279a16936a709125758cba24b17
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-24 05:03:57 -0800

    Add Comment Counter

--- src/js/module/callClass.js
+++ src/js/module/callClass.js
@@ -1,13 +1,16 @@
 import ItemCounter from './movieCounter.js';
 import InvolvementAPI from './InvolmentApi.js';
 import MovieApi from './movieApi.js';
+import CommentsCounter from './counterComment.js';
 
 const counter = new ItemCounter();
 const involvement = new InvolvementAPI();
 const movieApi = new MovieApi();
+const commentCounter = new CommentsCounter();
 
 export {
   counter,
+  commentCounter,
   involvement,
   movieApi,
 };
