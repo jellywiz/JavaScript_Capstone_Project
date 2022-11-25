39cb5394fe5540f831fb00af55a83ec6432bd81a
jellywiz <muzaffer.dler@gmail.com>
2022-11-25 14:09:54 +0300

    Update view movie to 12

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,4 @@
 import '../style.css';
-import displayImage from './view/displayImg.js';
 import displayMovies from './view/displaymovies.js';
 import addLikes from './module/addLikes.js';
 import renderLikes from './module/likesRender.js';
@@ -11,8 +10,7 @@ const moviesContainer = document.querySelector('.main-films');
 
 async function render() {
   document.onload = await addLikes();
-  document.onload = await displayImage(0);
-  document.onload = await displayMovies(20);
+  document.onload = await displayMovies(12);
   document.onload = await renderLikes();
   moviesContainer.addEventListener('click', (e) => {
     const { target } = e;
